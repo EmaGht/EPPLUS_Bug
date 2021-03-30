6921a8e83d73fb237f25037209061ec05e8d99c4
Emanuele Ghetti <emanuele.ghetti@energysoftware.it>
2021-03-30 12:49:16 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OfficeOpenXml;
+using System.IO;
+
+namespace EPPLUS_Bug
+{
+    class Program
+    {
+        static void Main()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var source = new FileInfo(@"./file.xlsx");
+            var destination = new FileInfo(@"./file_saved.xlsx");
+
+            using ExcelPackage excelPackage = new ExcelPackage(source);
+            excelPackage.SaveAs(destination);
+        }
+    }
+}
