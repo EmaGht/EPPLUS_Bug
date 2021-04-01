deeae311d49a7e319e12898e6a5e4b5c89433cec
Emanuele Ghetti <emanuele.ghetti@energysoftware.it>
2021-04-01 13:29:53 +0200

    - Updated epplus version with prerelease one - Added the next bug :)

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace EPPLUS_Bug
             var destination = new FileInfo(@"./file_saved.xlsx");
 
             using ExcelPackage excelPackage = new ExcelPackage(source);
+
+            //Setting this breaks the pinned headers when all i want it to do is just scroll the view into A56
+            //It also breaks the ability to scroll up for all cells before the 56th
+            excelPackage.Workbook.Worksheets[0].View.TopLeftCell = "A56";
+
             excelPackage.SaveAs(destination);
         }
     }
