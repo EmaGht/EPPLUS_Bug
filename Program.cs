e91704885c2ea51b8e2afcf3e40f9d35dc86308b
Emanuele Ghetti <emanuele.ghetti@energysoftware.it>
2021-04-09 12:40:57 +0200

    Changelog: - Added expected result with COMs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using OfficeOpenXml;
+﻿using Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
+using System;
 using System.IO;
 
 namespace EPPLUS_Bug
@@ -7,18 +9,41 @@ namespace EPPLUS_Bug
     {
         static void Main()
         {
+            #region Test setup
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             var source = new FileInfo(@"./file.xlsx");
             var destination = new FileInfo(@"./file_saved.xlsx");
+            var destination_expected = new FileInfo(@"./file_saved_expected.xlsx");
 
+            if (File.Exists(destination.FullName))
+                File.Delete(destination.FullName);
+
+            if (File.Exists(destination_expected.FullName))
+                File.Delete(destination_expected.FullName);
+            #endregion
+
+            // EPPLUS TEST
             using ExcelPackage excelPackage = new ExcelPackage(source);
-
-            //Setting this breaks the pinned headers when all i want it to do is just scroll the view into A56
-            //It also breaks the ability to scroll up for all cells before the 56th
             excelPackage.Workbook.Worksheets[0].View.TopLeftCell = "A56";
-
             excelPackage.SaveAs(destination);
+
+            // EXCEL COM TEST
+            DoExcelComTest(source, destination_expected);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        //Helper method so we don't Zombify the Excel process during debug
+        private static void DoExcelComTest(FileInfo source, FileInfo destination_expected)
+        {
+            Application xlApplication = new Application();
+            Workbook workBook = xlApplication.Workbooks.Open(source.FullName);
+            Worksheet workSheet = (Worksheet)workBook.Sheets[1];
+
+            workSheet.Range["A56", "A56"].Select();
+            workBook.SaveAs(destination_expected.FullName);
+            xlApplication.Quit();
         }
     }
 }
